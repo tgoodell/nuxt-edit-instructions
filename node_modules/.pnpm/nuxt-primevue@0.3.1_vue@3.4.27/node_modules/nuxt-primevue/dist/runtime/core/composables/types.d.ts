980fe77cd74f85b719eb7a80a808c1da8ed43ef0
Tristan Goodell <>
2024-05-29 14:19:15 -0400

    Reorganized files

--- node_modules/.pnpm/nuxt-primevue@0.3.1_vue@3.4.27/node_modules/nuxt-primevue/dist/runtime/core/composables/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConstructsType, ItemType } from '../types';
-
-export interface ComposablesType extends Omit<ConstructsType, 'prefix'> {}
-
-export interface ComposableType extends ItemType {}
--- node_modules/.pnpm/nuxt-primevue@0.3.1_vue@3.4.27/node_modules/nuxt-primevue/dist/runtime/core/directives/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConstructsType, ItemType } from '../types';
-
-export interface DirectivesType extends ConstructsType {}
-
-export interface DirectiveType extends ItemType {}
