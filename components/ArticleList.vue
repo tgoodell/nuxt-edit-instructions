4438a83006e31741b61f750f74604d4d0450c8bf
Tristan Goodell <>
2024-06-03 10:08:25 -0400

    Attempt at trying to use NuxtErrorBoundary correctly

---
+++ components/ArticleList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const helpArticleApi = useHelpArticleApi()
+const { data, error } = await helpArticleApi.list()
+
+if (error) {
+    throw new Error('Bob')
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="article in data"><HLink :to="`/admin/help/${article.slug}`">{{ article.title }}</HLink></li>
+    </ul>
+</template>
--- pages/admin/help/index.vue
+++ pages/admin/help/index.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-const helpArticleApi = useHelpArticleApi()
-const { data, error } = await helpArticleApi.list()
+function someErrorLogger(err) {
+  console.log('got an error', err);
+}
 </script>
 
 <template>
+  
   <PageWrapper class="max-w-screen-lg m-auto">
     <PageHeader page-title="Help Articles" />
     <AdminMenubar class="mb-3 mt-1" />
     <div class="border-2 p-2">
-      <ul v-if="!error">
-        <li v-for="article in data"><HLink :to="`/admin/help/${article.slug}`">{{ article.title }}</HLink></li>
-      </ul>
-      <NuxtErrorBoundary @error="error">
-        <h2 class="text-2xl semibold">Error: Unable to Fetch Articles</h2>
-        <p>Unable to fetch list of articles due to a network error.</p>
+      <NuxtErrorBoundary @error="someErrorLogger">
+        <ArticleList :data="data" />
+        <!-- <template #error="{ error, clearError }"> -->
+          <!-- <h2 class="text-2xl semibold">Error: Unable to Fetch Articles</h2>
+          <p>Unable to fetch list of articles due to a network error.</p> -->
+          <!-- {{error}} -->
+        <!-- </template> -->
+        <template #error="ctx"> An error happened. </template>
       </NuxtErrorBoundary>
     </div>
   </PageWrapper>
